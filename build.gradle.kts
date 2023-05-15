d470d4a29a124a6464bc455ebb0fc327f6739bd8
Apollo Zhu <97266506+zhuzhiyu-1962988@users.noreply.github.com>
2023-05-15 07:36:27 -0700

    Setup Project (#1)
    Co-authored-by: Apollo Zhu <zhuzhiyu@cs.washington.edu>

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("java")
+    jacoco
+}
+
+group = "edu.uw.cse403"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+    reports {
+        xml.required.set(true)
+    }
+}
